535ac3d11a532e3f8fcf18134bc6136b394cd597
Raoul Schmidiger <schmidiger@getbutik.com>
2018-05-14 16:46:41 +0200

    fixes

--- src/raoulsson/rcurl/RCurl.php
+++ src/raoulsson/rcurl/RCurl.php
@@ -17,236 +17,229 @@ use RuntimeException;
  */
 class RCurl {
 
-	/** @var LoggerInterface */
-	private $logger;
-	/** @var string */
-	private $userName;
-	/** @var string */
-	private $password;
+    /** @var LoggerInterface */
+    private $logger;
+    /** @var string */
+    private $userName;
+    /** @var string */
+    private $password;
 
-	/**
-	 * Curl constructor.
-	 *
-	 * @param $logger LoggerInterface
-	 */
-	public function __construct($logger = null) {
-		if($logger == null) {
-			$this->logger = new NullLogger();
-		} else {
-			$this->logger = $logger;
-		}
-	}
+    /**
+     * Curl constructor.
+     *
+     * @param $logger LoggerInterface
+     */
+    public function __construct($logger = null) {
+        if($logger == null) {
+            $this->logger = new NullLogger();
+        } else {
+            $this->logger = $logger;
+        }
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getUserName() {
-		return $this->userName;
-	}
+    /**
+     * @return string
+     */
+    public function getUserName() {
+        return $this->userName;
+    }
 
-	/**
-	 * @param string $userName
-	 */
-	public function setUserName($userName) {
-		$this->userName = $userName;
-	}
+    /**
+     * @param string $userName
+     */
+    public function setUserName($userName) {
+        $this->userName = $userName;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getPassword() {
-		return $this->password;
-	}
+    /**
+     * @return string
+     */
+    public function getPassword() {
+        return $this->password;
+    }
 
-	/**
-	 * @param string $password
-	 */
-	public function setPassword($password) {
-		$this->password = $password;
-	}
+    /**
+     * @param string $password
+     */
+    public function setPassword($password) {
+        $this->password = $password;
+    }
 
-	/**
-	 * @param $url
-	 * @param null $getFields
-	 * @param array|string $headerData
-	 * @param bool $debug
-	 * @return array
-	 */
-	public function makeGetCall($url, $getFields = null, array $headerData = null, $debug = false) {
-		return $this->doMakeCall("GET", $url, $getFields, null, $headerData, $debug);
-	}
+    /**
+     * @param $url
+     * @param null $getFields
+     * @param array|string $headerData
+     * @param bool $debug
+     * @return array
+     */
+    public function makeGetCall($url, $getFields = null, array $headerData = null, $debug = false) {
+        return $this->doMakeCall("GET", $url, $getFields, null, $headerData, $debug);
+    }
 
-	/**
-	 * @param $url
-	 * @param $postFields
-	 * @param array|string $headerData
-	 * @param bool $debug
-	 * @return array
-	 */
-	public function makePostCall($url, $postFields = null, array $headerData = null, $debug = false) {
-		return $this->doMakeCall("POST", $url, null, $postFields, $headerData, $debug);
-	}
+    /**
+     * @param $url
+     * @param $postFields
+     * @param array|string $headerData
+     * @param bool $debug
+     * @return array
+     */
+    public function makePostCall($url, $postFields = null, array $headerData = null, $debug = false) {
+        return $this->doMakeCall("POST", $url, null, $postFields, $headerData, $debug);
+    }
 
+    /**
+     * Back and forth the params but for client code convenience we do it
+     *
+     * @param $url
+     * @param array $excludeFields
+     * @param array $addGetFields
+     * @param bool $debug
+     * @return array
+     */
+    public function makeGetProxyCall($url, array $excludeFields = [], array $addGetFields = [], $debug = false) {
+        $uri = explode("?", $_SERVER["REQUEST_URI"]);
+        $url .= $uri[0];
+        $getFields = [];
+        if(count($uri) > 1) {
+            $kvs = explode("&", $uri[1]);
+            foreach($kvs as $kv) {
+                $lr = explode("=", $kv);
+                if(!in_array($lr[0], $excludeFields, false)) {
+                    $getFields[$lr[0]] = $lr[1];
+                }
+            }
+        }
+        $getFields = array_merge($getFields, $addGetFields);
+        return $this->doMakeCall("GET", $url, $getFields, null, null, $debug);
+    }
 
+    /**
+     * @param $method
+     * @param $url
+     * @param null $getFields
+     * @param $postFields
+     * @param array|string $headerData
+     * @param bool $debug
+     * @return array
+     */
+    private function doMakeCall($method, $url, $getFields = null, $postFields = null, array $headerData = null, $debug = false) {
+        if($debug) {
+            $this->logger->debug("Curl to: " . $url);
+        }
 
-	/**
-	 * @param $url
-	 * @param array|string $headerData
-	 * @param bool $debug
-	 * @param bool $allowParams
-	 * @return array
-	 */
-	public function makeSimpleCall($url, array $headerData = null, $debug = false, $allowParams = true) {
-		return $this->doMakeCall("GET", $url, null, null, $headerData, $debug, $allowParams);
-	}
+        if(self::contains($url, "?")) {
+            throw new RuntimeException("URL has to be clean of params, no ?");
+        }
 
-	/**
-	 * Back and forth the params but for client code convenience we do it
-	 *
-	 * @param $url
-	 * @param array $excludeFields
-	 * @param array $addGetFields
-	 * @param bool $debug
-	 * @return array
-	 */
-	public function makeGetProxyCall($url, array $excludeFields = [], array $addGetFields = [], $debug = false) {
-		$uri = explode("?", $_SERVER["REQUEST_URI"]);
-		$url .= $uri[0];
-		$getFields = [];
-		if(count($uri) > 1) {
-			$kvs = explode("&", $uri[1]);
-			foreach($kvs as $kv) {
-				$lr = explode("=", $kv);
-				if(!in_array($lr[0], $excludeFields, false)) {
-					$getFields[$lr[0]] = $lr[1];
-				}
-			}
-		}
-		$getFields = array_merge($getFields, $addGetFields);
-		return $this->doMakeCall("GET", $url, $getFields, null, null, $debug);
-	}
+        $ch = curl_init();
 
-	/**
-	 * @param $method
-	 * @param $url
-	 * @param null $getFields
-	 * @param $postFields
-	 * @param array|string $headerData
-	 * @param bool $debug
-	 * @return array
-	 */
-	private function doMakeCall($method, $url, $getFields = null, $postFields = null, array $headerData = null, $debug = false, $allowParams = false) {
-		if($debug) {
-			$this->logger->debug("Curl to: " . $url);
-		}
+        if($method == "GET") {
+            $url .= $this->appendGetFields($getFields);
+        }
 
-		if(!$allowParams || self::contains($url, "?")) {
-			throw new RuntimeException("URL has to be clean of params, no ?");
-		}
+        curl_setopt($ch, CURLOPT_URL, $url);
 
-		$ch = curl_init();
+        if($method == "POST") {
+            curl_setopt($ch, CURLOPT_POST, 1);
+        }
 
-		if($method == "GET") {
-			$url .= $this->appendGetFields($getFields);
-		}
+        if($method == "POST" && $postFields) {
+            if (is_array($postFields)) {
+                $query = http_build_query($postFields, '', '&');
+            } else {
+                $query = $postFields;
+            }
 
-		curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $query);
+            if(!$headerData) {
+                $headerData = [];
+            }
+            if(!$query) {
+                $query = "";
+            }
+            $headerData[] = "Content-Length: " . strlen($query);
+        }
 
-		if($method == "POST") {
-			curl_setopt($ch, CURLOPT_POST, 1);
-		}
+        if($headerData) {
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headerData);
+        }
 
-		if($method == "POST" && $postFields) {
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
-			if(!$headerData) {
-				$headerData = [];
-			}
-			if(!$postFields) {
-				$postFields = "";
-			}
-            $headerData[] = "Content-Length: " . strlen(json_encode($postFields));
-		}
+        if($this->userName && $this->password) {
+            curl_setopt($ch, CURLOPT_USERPWD, $this->userName . ":" . $this->password);
+        }
 
-		if($headerData) {
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headerData);
-		}
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 20);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 20);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_HEADER, true);
+        curl_setopt($ch, CURLOPT_USERAGENT, "rcurl");
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 
-		if($this->userName && $this->password) {
-			curl_setopt($ch, CURLOPT_USERPWD, $this->userName . ":" . $this->password);
-		}
+        $fp = null;
+        $tmpLog = null;
+        if($debug) {
+            $tmpLog = tempnam(sys_get_temp_dir(), 'crl');
+            $fp = fopen($tmpLog, 'wb');
+            curl_setopt($ch, CURLOPT_VERBOSE, true);
+            curl_setopt($ch, CURLOPT_STDERR, $fp);
+        }
 
-		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 20);
-		curl_setopt($ch, CURLOPT_TIMEOUT, 20);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-		curl_setopt($ch, CURLOPT_HEADER, true);
-		curl_setopt($ch, CURLOPT_USERAGENT, "rcurl");
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        $response = curl_exec($ch);
 
-		$fp = null;
-		$tmpLog = null;
-		if($debug) {
-			$tmpLog = tempnam(sys_get_temp_dir(), 'crl');
-			$fp = fopen($tmpLog, 'wb');
-			curl_setopt($ch, CURLOPT_VERBOSE, true);
-			curl_setopt($ch, CURLOPT_STDERR, $fp);
-		}
+        $headerSize = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
-		$response = curl_exec($ch);
+        curl_close($ch);
 
-		$headerSize = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-		$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $header = substr($response, 0, $headerSize);
+        $body = substr($response, $headerSize);
 
-		curl_close($ch);
+        if($debug) {
+            fclose($fp);
+            $fp = fopen($tmpLog, 'rb');
+            if ($fp) {
+                while(($line = fgets($fp)) !== false) {
+                    $this->logger->debug("CurlHeader: " . trim(preg_replace('/\s\s+/', ' ', $line)));
+                }
+            }
+            fclose($fp);
+            unlink($tmpLog);
+            $this->logger->debug("CurlBody: " . $body);
+        }
 
-		$header = substr($response, 0, $headerSize);
-		$body = substr($response, $headerSize);
+        return [
+            "code" => $httpCode,
+            "header" => $header,
+            "body"   => $body,
+        ];
+    }
 
-		if($debug) {
-			fclose($fp);
-			$fp = fopen($tmpLog, 'rb');
-			if ($fp) {
-				while(($line = fgets($fp)) !== false) {
-					$this->logger->debug("CurlHeader: " . trim(preg_replace('/\s\s+/', ' ', $line)));
-				}
-			}
-			fclose($fp);
-			unlink($tmpLog);
-			$this->logger->debug("CurlBody: " . $body);
-		}
+    /**
+     * @param $getFields
+     * @return null|string
+     */
+    private function appendGetFields($getFields) {
+        if($getFields) {
+            $sb = "";
+            $del = "?";
+            foreach($getFields as $k => $v) {
+                $sb .= $del . $k . "=" . rawurlencode($v);
+                $del = "&";
+            }
+            return $sb;
+        }
+        return null;
+    }
 
-		return [
-			"code" => $httpCode,
-			"header" => $header,
-			"body"   => $body,
-		];
-	}
-
-	/**
-	 * @param $getFields
-	 * @return null|string
-	 */
-	private function appendGetFields($getFields) {
-		if($getFields) {
-			$sb = "";
-			$del = "?";
-			foreach($getFields as $k => $v) {
-				$sb .= $del . $k . "=" . rawurlencode($v);
-				$del = "&";
-			}
-			return $sb;
-		}
-		return null;
-	}
-
-	/**
-	 * @param $heystack
-	 * @param $needle
-	 *
-	 * @return bool
-	 */
-	private static function contains($heystack, $needle) {
-		return strpos($heystack, $needle) !== false;
-	}
+    /**
+     * @param $heystack
+     * @param $needle
+     *
+     * @return bool
+     */
+    private static function contains($heystack, $needle) {
+        return strpos($heystack, $needle) !== false;
+    }
 
 }
